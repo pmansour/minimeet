7e56725ff0ba6297446f0826533f464cfc522c59
Peter <pmansour112@gmail.com>
2020-10-18 12:29:59 -0700

    update_url points to update manifest, upload latest update manifest

--- publish.sh
+++ publish.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-SIGNING_KEY=$(gsutil cat gs://stg-minimeet-private/minimeet.pem)
+PRIVATE_BUCKET_NAME="stg-minimeet-private"
+PUBLIC_BUCKET_NAME="stg-minimeet-public"
+EXTENSION_APP_ID="inlcnegncobjeadgjlacjhkgdmoikncb"
+
+echo "Downloading signing key.."
+SIGNING_KEY=$(gsutil cat "gs://$PRIVATE_BUCKET_NAME/minimeet.pem")
+
+echo "Packaging extension.."
 google-chrome --disable-gpu --pack-extension=src/ --pack-extension-key=<(echo "$SIGNING_KEY")
 
-gsutil cp src.crx gs://stg-minimeet-public/inlcnegncobjeadgjlacjhkgdmoikncb/latest.crx
+echo "Uploading latest files.."
+gsutil cp updates.xml "gs://$PUBLIC_BUCKET_NAME/$EXTENSION_APP_ID/updates.xml"
+gsutil cp src.crx "gs://$PUBLIC_BUCKET_NAME/$EXTENSION_APP_ID/latest.crx"
+
+echo "Done."
