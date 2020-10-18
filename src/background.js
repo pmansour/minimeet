dbf30bc27826ab20f94cabf29959cc0701d4eb98
Peter <pmansour112@gmail.com>
2020-10-17 23:23:22 -0700

    Use Meets homepage + messages instead of dedicated page

--- src/background.js
+++ src/background.js
@@ -1,77 +1,45 @@
 /**
- * Runs on a specific meeting's pre-join page.
- * Periodically looks for the 'Join now' button and clicks it.
+ * Runs on extension startup.
  */
 
-// const API_KEY = 'AIzaSyBAOiSGCe1hy8uKjmS6V-yoClSvhn-thTA';
+const retryTimeoutMilliseconds = 2000;
 
-// function joinQueryParams(queryParams) {
-//     return Object.keys(queryParams)
-//         .reduce((str, key) => {
-//             if (!queryParams.hasOwnProperty(key)) {
-//                 return str;
-//             }
+function startMeeting(meetingId) {
+    chrome.tabs.create({url: `https://meet.google.com/${meetingId}`}, (tab) => {
+        chrome.tabs.executeScript(tab.id, {
+            file: 'joinMeeting.js',
+        });
+    });
+}
 
-//             if (str) {
-//                 str += '&';
-//             }
+function tryJoinNextMeeting(tabId) {
+    chrome.tabs.sendMessage(tabId, {action: 'getNextMeetingId'}, (response) => {
+        console.debug('Received response');
+        if (response.nextMeetingId) {
+            startMeeting(response.nextMeetingId);
+            chrome.tabs.remove(tabId);
+            return;
+        }
+        setTimeout(() => tryJoinNextMeeting(tabId), retryTimeoutMilliseconds);
+    });
+}
 
-//             return str + `${key}=${queryParams[key]}`;
-//         }, '');
-// }
-
-// async function findNextMeeting(token) {
-//     let init = {
-//         method: 'GET',
-//         async: true,
-//         headers: {
-//             Authorization: 'Bearer ' + token,
-//             'Content-Type': 'application/json',
-//         },
-//         'contentType': 'json',
-//     };
-
-//     queryParams = {
-//         key: API_KEY,
-//         timeMin: (new Date()).toISOString(),
-//         showDeleted: false,
-//         singleEvents: true,
-//         maxResults: 1,
-//         orderBy: 'startTime',
-//     }
-//     queryStr = joinQueryParams(queryParams);
-
-//     let response = await fetch(
-//         `https://www.googleapis.com/calendar/v3/calendars/primary/events?${queryStr}`,
-//         init
-//     );
-//     let json = await response.json();
-//     console.debug('Calendar API response:');
-//     console.debug(json);
-//     return json.items[0];
-// }
-
-// function goToMeeting(meetingId) {
-//     chrome.tabs.create({url: `https://meet.google.com/${meetingId}`}, (tab) => {
-//         chrome.tabs.executeScript(tab.id, {
-//             file: 'joinMeeting.js',
-//         });
-//     });
-// }
-
-// function joinFirstMeeting() {
-//     chrome.identity.getAuthToken({interactive: true}, async (token) => {
-//         let meeting = await findNextMeeting(token);
-//         goToMeeting(meeting.conferenceData.conferenceId);
-//     });
-// }
+function startMeetTab() {
+    chrome.tabs.create({ url: 'https://meet.google.com' }, (tab) => {
+        chrome.tabs.executeScript(tab.id, {
+            file: 'findMeeting.js',
+        }, () => {
+            tryJoinNextMeeting(tab.id);
+        });
+    });
+}
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.identity.getAuthToken({interactive: true});
 });
 chrome.runtime.onStartup.addListener(() => {
-    chrome.tabs.create({ url: 'meeting.html' });
+    startMeetTab();
 });
 chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.create({ url: 'meeting.html' });
+    startMeetTab();
 });
---
+++ src/findMeeting.js
@@ -0,0 +1,26 @@
+/**
+ * Runs on the Google Meets homepage and services requests from the background
+ * script to find the next meeting.
+ */
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (sender.tab) {
+        console.warn('Ignoring message received from other tab.');
+        return;
+    }
+
+    switch (request.action) {
+        case 'getNextMeetingId':
+            const meetings = document.querySelectorAll('[data-call-id]');
+            if (!meetings || !meetings.length) {
+                console.debug('Could not find any meetings.');
+                sendResponse({nextMeetingId: null});
+                return;
+            }
+
+            sendResponse({nextMeetingId: meetings[0].dataset['callId']});
+            break;
+        default:
+            console.error('Received unknown request action.');
+    }
+})
