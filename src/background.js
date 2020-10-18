696086fe8b565a4e64a486b770d7ecae5de436b2
Peter <pmansour112@gmail.com>
2020-10-17 22:50:17 -0700

    Add logic to find ongoing meeting.

--- src/background.js
+++ src/background.js
@@ -3,64 +3,75 @@
  * Periodically looks for the 'Join now' button and clicks it.
  */
 
-const API_KEY = 'AIzaSyBAOiSGCe1hy8uKjmS6V-yoClSvhn-thTA';
+// const API_KEY = 'AIzaSyBAOiSGCe1hy8uKjmS6V-yoClSvhn-thTA';
 
-function joinQueryParams(queryParams) {
-    return Object.keys(queryParams)
-        .reduce((str, key) =>
-            str + (queryParams.hasOwnProperty(key) ? (str.length ? '&' : '') + queryParams[key] : ''));
-}
+// function joinQueryParams(queryParams) {
+//     return Object.keys(queryParams)
+//         .reduce((str, key) => {
+//             if (!queryParams.hasOwnProperty(key)) {
+//                 return str;
+//             }
 
-async function getNextMeeting(token) {
-    let init = {
-        method: 'GET',
-        async: true,
-        headers: {
-            Authorization: 'Bearer ' + token,
-            'Content-Type': 'application/json',
-        },
-        'contentType': 'json',
-    };
+//             if (str) {
+//                 str += '&';
+//             }
 
-    queryParams = {
-        key: API_KEY,
-        timeMin: (new Date()).toISOString(),
-        showDeleted: false,
-        maxResults: 1,
-        orderBy: 'startTime',
-    }
-    queryStr = joinQueryParams(queryParams);
+//             return str + `${key}=${queryParams[key]}`;
+//         }, '');
+// }
 
-    let response = await fetch(
-        `https://www.googleapis.com/calendar/v3/calendars/primary/events?${queryStr}`,
-        init
-    );
-    let json = await response.json();
-    console.log(json);
-    return json.items[0];
-}
+// async function findNextMeeting(token) {
+//     let init = {
+//         method: 'GET',
+//         async: true,
+//         headers: {
+//             Authorization: 'Bearer ' + token,
+//             'Content-Type': 'application/json',
+//         },
+//         'contentType': 'json',
+//     };
 
-function goToMeeting(meetingId) {
-    chrome.tabs.create({url: `https://meet.google.com/${meetingId}`}, (tab) => {
-        chrome.tabs.executeScript(tab.id, {
-            file: 'joinMeeting.js',
-        });
-    });
-}
+//     queryParams = {
+//         key: API_KEY,
+//         timeMin: (new Date()).toISOString(),
+//         showDeleted: false,
+//         singleEvents: true,
+//         maxResults: 1,
+//         orderBy: 'startTime',
+//     }
+//     queryStr = joinQueryParams(queryParams);
 
-function joinFirstMeeting() {
-    chrome.identity.getAuthToken({interactive: true}, async (token) => {
-        let meeting = await getNextMeeting(token);
-        goToMeeting(meeting.conferenceData.conferenceId);
-    });
-}
+//     let response = await fetch(
+//         `https://www.googleapis.com/calendar/v3/calendars/primary/events?${queryStr}`,
+//         init
+//     );
+//     let json = await response.json();
+//     console.debug('Calendar API response:');
+//     console.debug(json);
+//     return json.items[0];
+// }
+
+// function goToMeeting(meetingId) {
+//     chrome.tabs.create({url: `https://meet.google.com/${meetingId}`}, (tab) => {
+//         chrome.tabs.executeScript(tab.id, {
+//             file: 'joinMeeting.js',
+//         });
+//     });
+// }
+
+// function joinFirstMeeting() {
+//     chrome.identity.getAuthToken({interactive: true}, async (token) => {
+//         let meeting = await findNextMeeting(token);
+//         goToMeeting(meeting.conferenceData.conferenceId);
+//     });
+// }
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.identity.getAuthToken({interactive: true});
 });
 chrome.runtime.onStartup.addListener(() => {
-    joinFirstMeeting();
+    chrome.tabs.create({ url: 'meeting.html' });
 });
 chrome.browserAction.onClicked.addListener(() => {
-    joinFirstMeeting();
+    chrome.tabs.create({ url: 'meeting.html' });
 });
--- src/meeting.js
+++ src/meeting.js
@@ -1,51 +1,110 @@
+/**
+ * Runs on startup. Tries to start 
+ * Periodically looks for the 'Join now' button and clicks it.
+ */
+
 const API_KEY = 'AIzaSyBAOiSGCe1hy8uKjmS6V-yoClSvhn-thTA';
 
 function joinQueryParams(queryParams) {
     return Object.keys(queryParams)
-        .reduce((str, key) =>
-            str + (queryParams.hasOwnProperty(key) ? (str.length ? '&' : '') + queryParams[key] : ''));
+        .reduce((str, key) => {
+            if (!queryParams.hasOwnProperty(key)) {
+                return str;
+            }
+
+            if (str) {
+                str += '&';
+            }
+
+            return str + `${key}=${queryParams[key]}`;
+        }, '');
 }
 
-export async function getNextMeeting(token) {
+async function listEvents(token, queryParams) {
     let init = {
         method: 'GET',
         async: true,
         headers: {
-            Authorization: 'Bearer ' + token,
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
         'contentType': 'json',
     };
-
-    queryParams = {
-        key: API_KEY,
-        timeMin: (new Date()).toISOString(),
-        showDeleted: false,
-        maxResults: 1,
-        orderBy: 'startTime',
-    }
     queryStr = joinQueryParams(queryParams);
-
+    console.debug(queryStr);
     let response = await fetch(
         `https://www.googleapis.com/calendar/v3/calendars/primary/events?${queryStr}`,
         init
     );
-    let json = await response.json();
-    console.log(json);
-    return json.items[0];
+    return response.json();
 }
 
-export function goToMeeting(meetingId) {
+async function findOngoingMeeting(token) {
+    queryParams = {
+        key: API_KEY,
+        timeMin: new Date(new Date().getDate() - 1).toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        orderBy: 'startTime',
+    }
+
+    let meetings = await listEvents(token, queryParams);
+    console.debug('Calendar API response:');
+    console.debug(meetings);
+
+    ongoingMeetings = meetings.items.filter(meeting => new Date(meeting.end.dateTime) > new Date());
+    if (ongoingMeetings.length) {
+        return ongoingMeetings[0];
+    }
+    return null;
+}
+
+async function findNextMeeting(token) {
+    queryParams = {
+        key: API_KEY,
+        timeMin: (new Date()).toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 1,
+        orderBy: 'startTime',
+    }
+
+    let meetings = await listEvents(token, queryParams);
+    console.debug('Calendar API response:');
+    console.debug(meetings);
+    return meetings.items[0];
+}
+
+function goToMeeting(meetingId) {
     chrome.tabs.create({url: `https://meet.google.com/${meetingId}`}, (tab) => {
-
+        chrome.tabs.executeScript(tab.id, {
+            file: 'joinMeeting.js',
+        });
+        // closeCurrentTab();
     });
 }
 
-window.onload = () => {
-    document.querySelector('button').addEventListener('click', () => {
-        chrome.identity.getAuthToken({interactive: true}, async (token) => {
-            let meeting = await getNextMeeting(token);
-            document.querySelector('div').innerText = meeting.conferenceData.conferenceId;
-        });
+function showNoMeetingMessage() {
+    console.log('No meetings available');
+}
+
+function joinFirstMeeting() {
+    chrome.identity.getAuthToken({interactive: true}, async (token) => {
+        let meeting = await findOngoingMeeting(token);
+        if (!meeting) {
+            meeting = findNextMeeting(token);
+            if (!meeting) {
+
+            }
+        }
+        // let meeting = await findNextMeeting(token);
+        goToMeeting(meeting.conferenceData.conferenceId);
     });
-};
+}
+
+function closeCurrentTab() {
+    chrome.tabs.getCurrent((tab) => {
+        chrome.tabs.remove(tab.id);
+    });
+}
+joinFirstMeeting();
