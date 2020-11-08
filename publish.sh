281748d8e31a7c93e429509a4db74a7047fd0efc
Peter Mansour <pmansour112@gmail.com>
2020-11-08 15:08:48 -0800

    update publishing script for WSL

--- publish.sh
+++ publish.sh
@@ -4,11 +4,14 @@ PRIVATE_BUCKET_NAME="stg-minimeet-private"
 PUBLIC_BUCKET_NAME="stg-minimeet-public"
 EXTENSION_APP_ID="inlcnegncobjeadgjlacjhkgdmoikncb"
 
+CHROME_BIN="google-chrome" # For linux
+# CHROME_BIN="/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe" # For WSL
+
 echo "Downloading signing key.."
 SIGNING_KEY=$(gsutil cat "gs://$PRIVATE_BUCKET_NAME/minimeet.pem")
 
 echo "Packaging extension.."
-google-chrome --disable-gpu --pack-extension=src/ --pack-extension-key=<(echo "$SIGNING_KEY")
+"$CHROME_BIN" --disable-gpu --pack-extension=src/ --pack-extension-key=<(echo "$SIGNING_KEY")
 
 echo "Uploading latest files.."
 gsutil cp updates.xml "gs://$PUBLIC_BUCKET_NAME/$EXTENSION_APP_ID/updates.xml"
