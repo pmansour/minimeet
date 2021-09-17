44792cf34e693f9feb7db4bfc9459a85c6989305
Peter Mansour <pmansour112@gmail.com>
2021-09-16 18:40:55 -0700

    load credentials from file, reduce login timeout

--- src2/content/login.js
+++ src2/content/login.js
@@ -1,20 +1,18 @@
+import * as creds from '/config/creds.js';
 import { getLoginState, LOGIN_STATE, clickProfileIdentifier, clickSwitchAccount, clickUseAnotherAccount, enterEmail, enterPassword } from '../services/login.js';
 import { info, debug } from '/util/logging.js';
 
 info('Hello from the login script!');
 
-const EMAIL = 'foo@bar.com';
-const PASSWORD = 'pass123';
-
 function doLogin() {
     info('Starting login flow..');
-    const state = getLoginState(EMAIL);
+    const state = getLoginState(creds.EMAIL_ADDRESS);
     info(`Login state: ${state}`);
     
     switch (state) {
         case LOGIN_STATE.LOGGED_OUT_SAME_ACCOUNT:
             debug('Choosing existing profile..');
-            clickProfileIdentifier(EMAIL);
+            clickProfileIdentifier(creds.EMAIL_ADDRESS);
             break;
         case LOGIN_STATE.LOGGED_OUT_NO_ACCOUNT:
             debug('Choosing another account..');
@@ -22,11 +20,11 @@ function doLogin() {
             break;
         case LOGIN_STATE.ENTER_EMAIL:
             debug('Entering email address..');
-            enterEmail(EMAIL);
+            enterEmail(creds.EMAIL_ADDRESS);
             break;
         case LOGIN_STATE.ENTER_PASSWORD_SAME_ACCOUNT:
             debug('Entering password..');
-            enterPassword(PASSWORD);
+            enterPassword(creds.PASSWORD);
             break;
         case LOGIN_STATE.ENTER_PASSWORD_WRONG_ACCOUNT:
             debug('Switching accounts..');
@@ -38,4 +36,4 @@ function doLogin() {
     }
 }
 
-setInterval(doLogin, 3000);
+setInterval(doLogin, 2000);
