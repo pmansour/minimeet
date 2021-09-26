6b10665c8cbe5ad3369d2dcd7bdabbfc1b31f47b
Peter Mansour <pmansour112@gmail.com>
2021-09-26 15:49:29 -0700

    ensure bin directory exists

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 WORKING_DIR="$(realpath "$(dirname "$0")")"
 
+mkdir -p "$WORKING_DIR/bin" >/dev/null
+
 echo "Publishing mv3 extension.."
 pushd "$WORKING_DIR/mv3" >/dev/null && zip -r "$WORKING_DIR/bin/mv3.zip" >/dev/null ./ && popd >/dev/null
 gh release upload stable "$WORKING_DIR/bin/mv3.zip" --clobber
