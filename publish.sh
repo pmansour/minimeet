ab8e4706a3b9f28bb6301e11bbd1694a1d8df325
Peter Mansour <pmansour112@gmail.com>
2021-09-26 15:47:40 -0700

    added script to publish extension binaries

---
+++ publish.sh
@@ -0,0 +1,11 @@
+WORKING_DIR="$(realpath "$(dirname "$0")")"
+
+echo "Publishing mv3 extension.."
+pushd "$WORKING_DIR/mv3" >/dev/null && zip -r "$WORKING_DIR/bin/mv3.zip" ./ && popd >/dev/null
+gh release upload stable "$WORKING_DIR/bin/mv3.zip" --clobber
+
+# echo "Publishing mv2 extension.."
+# pushd "$WORKING_DIR/mv2" >/dev/null && zip -r "$WORKING_DIR/bin/mv2.zip" ./ && popd >/dev/null
+# gh release upload stable "$WORKING_DIR/bin/mv2.zip" --clobber
+
+echo "Done."
