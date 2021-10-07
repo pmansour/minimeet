79c6f8112ac1ebf5997bc90562e4463d1f9000c8
Peter Mansour <pmansour112@gmail.com>
2021-10-07 10:56:38 -0700

    add jasmine and stub (not working) test

---
+++ spec/util/url.spec.mjs
@@ -0,0 +1,18 @@
+import urlLib from '../../src/util/url.js';
+const { getLoginRedirectUrl } = urlLib;
+
+describe("getLoginRedirectUrl", function() {
+  it("should support URL scheme for the continuation URL", function() {
+    const redirectUrl = new URL(getLoginRedirectUrl('https://google.com'));
+    expect(redirectUrl.searchParams.get('continue')).toEqual('https://google.com');
+  });
+
+  describe("when redirecting to meet.google.com", function() {
+    const continuationUrl = 'meet.google.com';
+
+    it("should URL encode the continuation URL", function() {
+      const redirectUrl = new URL(getLoginRedirectUrl(continuationUrl));
+      expect(redirectUrl.search).toContain(encodeURIComponent(continuationUrl));
+    });
+  });
+});
