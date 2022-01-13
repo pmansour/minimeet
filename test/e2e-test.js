061620fb8656495565eb34d5c2f548400aed72fb
Peter Mansour <pmansour112@gmail.com>
2022-01-13 09:24:37 -0800

    basic test that starts

--- test/e2e-test.js
+++ test/e2e-test.js
@@ -2,17 +2,16 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const extensionPath = require('path').join(__dirname, '../mv3');
-    const browser = puppeteer.launch({
+    const browser = await puppeteer.launch({
         headless: false,
         args: [
             `--disable-extensions-except=${extensionPath}`,
             `--load-extension=${extensionPath}`,
         ]
     });
-
     
     const page = await browser.newPage();
-    await page.goto('https://www.google.com');
+    await page.goto('https://meet.google.com');
     
     await browser.close();
 })();
