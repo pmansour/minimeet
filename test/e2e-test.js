29dbf1b926bbeaa9adb40274b64baed6131c3d6a
Peter Mansour <pmansour112@gmail.com>
2022-01-12 16:37:39 -0800

    flesh out test code

--- test/e2e-test.js
+++ test/e2e-test.js
@@ -1,4 +1,18 @@
 const puppeteer = require('puppeteer');
 
+(async () => {
+    const extensionPath = require('path').join(__dirname, '../mv3');
+    const browser = puppeteer.launch({
+        headless: false,
+        args: [
+            `--disable-extensions-except=${extensionPath}`,
+            `--load-extension=${extensionPath}`,
+        ]
+    });
 
-let browser = puppeteer.launch({ headless: false });
+    
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com');
+    
+    await browser.close();
+})();
