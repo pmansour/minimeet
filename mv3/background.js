0670aa0a1845e1a5faf6bf090975bcab9728c0ad
Peter Mansour <pmansour112@gmail.com>
2022-02-20 11:21:47 -0800

    fix spelling mistake

--- mv3/background.js
+++ mv3/background.js
@@ -36,7 +36,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 });
 
 // Explicitly allow Google sites (in particular, Meet) to use necessary permissions.
-const allowedSiteSettings = [chrome.contentSettings.notificatoins, chrome.contentSettings.camera, chrome.contentSettings.microphone];
+const allowedSiteSettings = [chrome.contentSettings.notifications, chrome.contentSettings.camera, chrome.contentSettings.microphone];
 allowedSiteSettings.forEach((setting) => {
     setting.set({
         primaryPattern: 'https://*.google.com/*',
