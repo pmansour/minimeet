9b6bc2b9a6840c2f06fb734b84f0d8bf20668e31
Peter Mansour <pmansour112@gmail.com>
2022-03-12 15:02:04 -0800

    refactor zoom e2e tests

--- test/zoom-e2e-test.js
+++ test/zoom-e2e-test.js
@@ -16,14 +16,20 @@ async function WaitForInMeeting(page, timeout = 30 * 1000) {
         });
 }
 
+function delay(timeoutMs) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, timeoutMs);
+    });
+}
+
 describe('minimeet extension', function() {
     const extensionPath = path.join(__dirname, '../mv3');
 
-    before('Load zoom config', function() {
+    before('load zoom config', function() {
         this.zoomConfig = getZoomConfig();
     });
 
-    beforeEach('Start browser', async function() {
+    before('start browser', async function() {
         this.timeout(20 * 1000);
         this.browser = await puppeteer.launch({
             headless: false,
@@ -34,22 +40,31 @@ describe('minimeet extension', function() {
         });
     });
 
-    afterEach('Close browser', async function() {
-        this.timeout(20 * 1000);
-        await this.browser.close();
+    before('start page', async function() {
+        this.page = await this.browser.newPage();
+        await this.page.goto(this.zoomConfig.testMeetingUrl);
     });
 
-    xit('Inserts room name successfully', function() {});
-    xit('Unmutes successfully', function() {});
-    xit('Starts video successfully', function() {});
-
-    it('Joins meeting successfully', async function() {
+    it('joins successfully', async function() {
         this.timeout(90 * 1000);
 
-        const page = await this.browser.newPage();
-        await page.goto(this.zoomConfig.testMeetingUrl);
+        await WaitForInMeeting(this.page);
+        assert.equal(await this.page.title(), this.zoomConfig.testMeetingTitle);
+    });
 
-        await WaitForInMeeting(page);
-        assert.equal(await page.title(), this.zoomConfig.testMeetingTitle);
+    it('sticks around', async function() {
+        this.timeout(90 * 1000);
+
+        await delay(5 * 1000);
+        assert.equal(await this.page.title(), this.zoomConfig.testMeetingTitle);
+    });
+
+    xit('inserts room name successfully', function() {});
+    xit('unmutes successfully', function() {});
+    xit('starts video successfully', function() {});
+
+    after('close browser', async function() {
+        this.timeout(20 * 1000);
+        await this.browser.close();
     });
 });
