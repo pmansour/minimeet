c6a1a39a54054e2dcb8622d6d4edeec1d99a2376
Peter Mansour <pmansour112@gmail.com>
2022-03-12 15:54:50 -0800

    refactored content script within zoom meeting

--- mv3/content/withinZoomMeeting.js
+++ mv3/content/withinZoomMeeting.js
@@ -1,4 +1,5 @@
-import { getElement, byButtonText, bySelector } from '/util/dom.js';
+import { getElement, byButtonText, bySelector, byVisibleSelfText } from '/util/dom.js';
+import { info } from '/util/logging.js';
 
 function joinAudioByComputer() {
     const btn = getElement(byButtonText('Join Audio by Computer'));
@@ -14,7 +15,47 @@ function dismissModal() {
     }
 }
 
+function audioIsReady() {
+    return !!getElement(byVisibleSelfText('Mute')) || !!getElement(byVisibleSelfText('Unmute'));
+}
+
+function unmuteAudio() {
+    const btn = getElement(byVisibleSelfText('Unmute'));
+    if (btn) {
+        btn.click();
+    }
+}
+
+function videoIsReady() {
+    return !!getElement(byVisibleSelfText('Start Video')) || !!getElement(byVisibleSelfText('Stop Video'));
+}
+
+function startVideo() {
+    const btn = getElement(byVisibleSelfText('Start Video'));
+    if (btn) {
+        btn.click();
+    }
+}
+
+var videoWasInitialized = false;
+var audioWasInitialized = false;
+
 setInterval(() => {
+    // These other tasks can be done repeatedly without harm.
     dismissModal();
     joinAudioByComputer();
-}, 2000);
+
+    // Only initialize these once at meeting startup, so that a human can later override them if needed.
+    if (!audioWasInitialized && audioIsReady()) {
+        unmuteAudio();
+        audioWasInitialized = true;
+        info('Minimeet: Unmuted microphone');
+    }
+    // Delay enabling video until after audio is initialized, since doing it too early sometimes
+    // doesn't work.
+    if (!videoWasInitialized && videoIsReady() && audioWasInitialized) {
+        startVideo();
+        videoWasInitialized = true;
+        info('Minimeet: Started video');
+    }
+}, 1000);
